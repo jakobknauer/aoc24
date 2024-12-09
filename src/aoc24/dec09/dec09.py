2023cf028b7a952af71396d5569d9da063d81ce8
Jakob Knauer <143888036+jakobknauer@users.noreply.github.com>
2024-12-09 17:02:26 +0100

    Day 9 (generalize both parts)

--- src/aoc24/dec09/dec09.py
+++ src/aoc24/dec09/dec09.py
@@ -11,14 +11,36 @@ def main() -> None:
     with open(sys.argv[1], "r") as fp:
         diskmap = list(map(int, fp.readline().strip()))
 
-    free_blocks, filled_blocks = decompress(diskmap)
-    # compact(disk)
-    compacted_disk = compact2(free_blocks, filled_blocks)
-    checksum = compute_checksum2(compacted_disk)
+    # free_blocks, filled_blocks = decompress_blocks(diskmap)  # Part 1
+    free_blocks, filled_blocks = decompress_files(diskmap)  # Part2
+    compact(free_blocks, filled_blocks)
+    checksum = compute_checksum(filled_blocks)
     print(checksum)
 
 
-def decompress(diskmap: list[int]) -> tuple[list[FreeBlock], list[FilledBlock]]:
+def decompress_blocks(diskmap: list[int]) -> tuple[list[FreeBlock], list[FilledBlock]]:
+    free_blocks: list[FreeBlock] = []
+    filled_blocks: list[FilledBlock] = []
+
+    free_block: bool = False
+    fileno: int = 0
+    current_pos = 0
+
+    for block_size in diskmap:
+        if free_block:
+            for i in range(block_size):
+                free_blocks.append((current_pos + i, 1))
+        else:
+            for i in range(block_size):
+                filled_blocks.append(((current_pos + i, 1), fileno))
+            fileno += 1
+        free_block = not free_block
+        current_pos += block_size
+
+    return free_blocks, filled_blocks
+
+
+def decompress_files(diskmap: list[int]) -> tuple[list[FreeBlock], list[FilledBlock]]:
     free_blocks: list[FreeBlock] = []
     filled_blocks: list[FilledBlock] = []
 
@@ -38,29 +60,8 @@ def decompress(diskmap: list[int]) -> tuple[list[FreeBlock], list[FilledBlock]]:
     return free_blocks, filled_blocks
 
 
-# def compact(disk: list[int, int | None]) -> None:
-#     empty_slot: int = 0
-#     filled_slot: int = len(disk) - 1
-#
-#     while disk[empty_slot] is not None:
-#         empty_slot += 1
-#
-#     while disk[filled_slot] is None:
-#         filled_slot -= 1
-#
-#     while empty_slot < filled_slot:
-#         disk[empty_slot], disk[filled_slot] = disk[filled_slot], disk[empty_slot]
-#         while disk[empty_slot] is not None:
-#             empty_slot += 1
-#
-#         while disk[filled_slot] is None:
-#             filled_slot -= 1
-
-
-def compact2(free_blocks: list[FreeBlock], filled_blocks: list[FilledBlock]) -> list[FilledBlock]:
-    compacted_disk: list[FilledBlock] = []
-
-    for (file_pos, file_size), file_id in reversed(filled_blocks):
+def compact(free_blocks: list[FreeBlock], filled_blocks: list[FilledBlock]) -> None:
+    for file_index, ((file_pos, file_size), file_id) in reversed(list(enumerate(filled_blocks))):
         found_free_block = False
         for free_block_index, (free_pos, free_size) in enumerate(free_blocks):
             if free_pos >= file_pos:
@@ -70,23 +71,17 @@ def compact2(free_blocks: list[FreeBlock], filled_blocks: list[FilledBlock]) -> 
                 break
 
         if not found_free_block:
-            compacted_disk.append(((file_pos, file_size), file_id))
+            filled_blocks[file_index] = ((file_pos, file_size), file_id)
         elif free_size == file_size:
-            compacted_disk.append(((free_pos, file_size), file_id))
+            filled_blocks[file_index] = ((free_pos, file_size), file_id)
             del free_blocks[free_block_index]
         else:
-            compacted_disk.append(((free_pos, file_size), file_id))
+            filled_blocks[file_index] = ((free_pos, file_size), file_id)
             del free_blocks[free_block_index]
             free_blocks.insert(free_block_index, (free_pos + file_size, free_size - file_size))
 
-    return compacted_disk
 
-
-# def compute_checksum(disk: list[int | None]) -> int:
-#     return sum(i * n for (i, n) in enumerate(disk) if n is not None)
-
-
-def compute_checksum2(disk: list[FilledBlock]) -> int:
+def compute_checksum(disk: list[FilledBlock]) -> int:
     checksum = 0
 
     for (pos, size), file_id in disk:
